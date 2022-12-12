50f9c74d46508583830f1127be43adcc8aaed1a6
Sam Stoelinga <sammiestoel@gmail.com>
2022-12-11 21:14:08 -0800

    Add templates and test data from SO

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from models import Entry, load_test_data
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    entries = Entry.select().order_by(Entry.created.desc()).limit(10)
+    return render_template("index.html", entries=entries)
+
+
+@app.route("/<int:post_id>-<string:path>")
+def entry(post_id, path):
+    entry = Entry.get_by_id(post_id)
+    return render_template("entry.html", entry=entry)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,26 +1,49 @@
 import datetime
 import os
+import sys
 
 from dotenv import load_dotenv
 from peewee import *
+from playhouse.db_url import connect
+import requests
+from slugify import slugify
 
 load_dotenv()
 
-from playhouse.db_url import connect
 
 # Connect to the database URL defined in the environment, falling
 # back to a local Sqlite database if no database URL is specified.
 db = connect(os.environ.get('DATABASE') or 'sqlite:///test.db')
 
+
 class Entry(Model):
     class Meta:
         database = db
-    slug = CharField(unique=True)
+    slug = TextField()
     prompt = TextField()
-    response = TextField()
-    created_date = DateTimeField(default=datetime.datetime.now)
+    title = TextField()
+    answer = TextField(null=True)
+    question_id = IntegerField(null=True)
+    question_body = TextField()
+    created = DateTimeField(default=datetime.datetime.now)
+
+
+def load_test_data():
+    Entry.delete()
+    questions = requests.get("https://api.stackexchange.com/2.3/questions?fromdate=1670630400&"
+                             "order=desc&max=1670716800&sort=activity&tagged=golang&site=stackoverflow"
+                             "&filter=!nOedRLbBQj").json()
+    for item in questions["items"]:
+        prompt = item["title"] + "<br/>" + item["body"]
+        Entry.get_or_create(slug=slugify(item["title"]), prompt=prompt,
+                            question_id=item["question_id"], question_body=item["body"], title=item["title"])
 
 
 if __name__ == "__main__":
-    db.connect()
-    db.create_tables([Entry])
+    if sys.argv[1] == "create_tables":
+        db.connect()
+        db.create_tables([Entry])
+    if sys.argv[1] == "load_test_data":
+        load_test_data()
+    if sys.argv[1] == "delete_all_data":
+        Entry.delete()
